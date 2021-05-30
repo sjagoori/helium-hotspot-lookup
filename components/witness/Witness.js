0360a99db2d7ba2d97360ad3bd753c0556163022
sjagoori <sjagoori@users.noreply.github.com>
2021-05-30 22:39:29 +0200

    ~ added key value to loop

--- components/witness/Witness.js
+++ components/witness/Witness.js
@@ -4,9 +4,9 @@ export default function Witness(props) {
   return (
     <section>
       <Header label="Witness" />
-      {Object.values(props.data).map((key) => {
+      {Object.values(props.data).map((key, index) => {
         return (
-          <a href={key.address}>
+          <a key={index} href={key.address}>
             <h2>{key.name}</h2>
             <h3>{key.geocode.long_street + ", " + key.geocode.long_city}</h3>
             <p>Status: {key.status.online}</p>
