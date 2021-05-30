74e8889fbaf6601aef06984672c1c6bb3013cdb7
sjagoori <sjagoori@users.noreply.github.com>
2021-05-30 16:00:38 +0200

    + street & city names

--- components/witness/Witness.js
+++ components/witness/Witness.js
@@ -8,6 +8,9 @@ export default function Witness(props) {
         return (
           <a href={key.address}>
             <h2>{key.name}</h2>
+            <h3>
+              {key.geocode.long_street + ", " + key.geocode.long_city}
+            </h3>
             <p>Status: {key.status.online}</p>
             <p>Scale {key.reward_scale}</p>
           </a>
