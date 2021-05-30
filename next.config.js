c95fb364e465826ec10d940abd50822e8096b51b
sjagoori <sjagoori@users.noreply.github.com>
2021-05-30 22:52:29 +0200

    ~

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 const withPWA = require("next-pwa");
 
-module.exports = withPWA({
-  pwa: {
-    dest: "public",
-  },
-});
-
 module.exports = {
+  withPWA: withPWA({
+    pwa: {
+      dest: "public",
+    },
+  }),
   future: {
     webpack5: true,
   },
