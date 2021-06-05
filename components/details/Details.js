065feaba098937bf7497a69214f751f9e2b820be
sjagoori <sjagoori@users.noreply.github.com>
2021-06-05 22:44:08 +0200

    + styling

--- components/details/Details.js
+++ components/details/Details.js
@@ -1,9 +1,10 @@
 import Header from '../header/Header'
+import Section from '../section/Section'
 
-export default function Details(props) {  
+export default function Details(props) {
   return (
-    <section>
-      <Header label="Details"/>
+    <Section>
+      <Header label="Details" />
       <h2>{props.data.name}</h2>
       <p>
         {props.data.geocode.long_street}, {props.data.geocode.long_state}
@@ -11,6 +12,6 @@ export default function Details(props) {
       <p>
         {props.data.geocode.long_city}, {props.data.geocode.long_country}
       </p>
-    </section>
+    </Section>
   );
 }
