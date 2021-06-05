87f1543b76e4500634a54e0d0835e4730fab8785
sjagoori <sjagoori@users.noreply.github.com>
2021-06-05 22:42:08 +0200

    + styling

--- components/section/Section.js
+++ components/section/Section.js
@@ -1,9 +1,11 @@
 import styles from './Section.module.css'
 
-export default function Section(props){
+export default function Section(props) {
   return (
     <section className={styles.container}>
-      {props.children}
+      <div className={styles.content}>
+        {props.children}
+      </div>
     </section>
   )
 }
