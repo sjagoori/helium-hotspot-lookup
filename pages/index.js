14c0f71aed56bc652a4a0df4b9cbd9035a2b1d1a
sjagoori <sjagoori@users.noreply.github.com>
2021-06-05 22:43:19 +0200

    + errorr state

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
@@ -6,33 +5,41 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   const router = useRouter();
   const [saves, setSaves] = useState([]);
-
-  // console.log(reactLocalStorage);
+  const [error, setError] = useState(null)
 
   function handleSubmit(e) {
     e.preventDefault();
     let hotspotAddress = e.target[0].value;
-    router.push({
-      pathname: `hotspot/${hotspotAddress}`,
-    });
+    if (hotspotAddress.length < 51) {
+      setError("Enter a valid hotspot address")
+      e.target[0].value = ""
+    } else {
+      router.push({
+        pathname: `hotspot/${hotspotAddress}`,
+      });
+    }
   }
 
-  useEffect(()=>{
-    Object.values(localStorage).map(key => { 
+  useEffect(() => {
+    Object.values(localStorage).map(key => {
       setSaves(saves => saves = localStorage[key])
     });
-    console.log(saves);  
+    console.log(saves);
   })
 
 
   return (
     <div className={styles.container}>
+      <img src="./icons/icon-192x192.png" alt="Icon" width="50" height="50" />
+      <h1>Hotspot lookup </h1>
       <form onSubmit={handleSubmit}>
+        {error ? <span className={styles.error}>{error}</span> : null}
         <input
           type="text"
           name="hotspotAddress"
-          placeholder="Enter your address"
+          placeholder="11cvkGZG4uYVKUoX5DJMp8Mh5FiGLW6Yv3avKNLMNuRqU6CLGMs"
           id="hotspotAddress"
+          className={error ? styles.error : null}
         />
         <button type="submit">Search</button>
       </form>
