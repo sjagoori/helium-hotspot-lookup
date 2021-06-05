37b3805145374f7cc1669992fb4eab88a5ebe13f
sjagoori <sjagoori@users.noreply.github.com>
2021-06-05 22:42:30 +0200

    + styling & semantics

--- components/rewards/Rewards.js
+++ components/rewards/Rewards.js
@@ -1,26 +1,27 @@
 import Header from "../header/Header";
+import Section from '../section/Section'
 
 export default function Rewards(props) {
   return (
-    <section>
+    <Section>
       <Header label="Rewards" />
-      <h2>Current price:</h2>
+      <h3>Current price:</h3>
       <span>${props.price}</span>
-      <h2>Today:</h2>
+      <h3>Today:</h3>
       <span>
         {props.data.today.data.total.toFixed(2)} HNT ($
         {(props.data.today.data.total * props.price).toFixed(2)})
       </span>
-      <h2>Last 7d:</h2>
+      <h3>Last 7d:</h3>
       <span>
         {props.data.weekly.data.total.toFixed(2)} HNT ($
         {(props.data.weekly.data.total * props.price).toFixed(2)})
       </span>
-      <h2>Last 30d:</h2>
+      <h3>Last 30d:</h3>
       <span>
         {props.data.monthly.data.total.toFixed(2)} HNT ($
         {(props.data.monthly.data.total * props.price).toFixed(2)})
       </span>
-    </section>
+    </Section>
   );
 }
