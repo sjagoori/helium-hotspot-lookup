c805e9deaacf7c6fcd7ddc27b7cdff9d5580281a
sjagoori <sjagoori@users.noreply.github.com>
2021-06-05 23:33:29 +0200

    + load state

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
+import React, { useState } from 'react';
+import Router from 'next/router';
+import LoadingScreen from '../components/loadingScreen/loadingScreen'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [loading, setLoading] = useState(false);
+
+  Router.onRouteChangeStart = () => {
+    setLoading(true)
+  }
+
+  Router.onRouteChangeComplete = (e) => {
+    setLoading(false)
+  }
+
+  Router.onRouteChangeError = () => {
+    // handle error state here
+  }
+
+  return loading ? <LoadingScreen /> : <Component {...pageProps} />
 }
 
 export default MyApp
