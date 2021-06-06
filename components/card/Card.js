04726bb990b81ee626c5bac7b1f9a3f73a41b4c7
sjagoori <sjagoori@users.noreply.github.com>
2021-06-06 20:34:02 +0200

    + secondary variant

--- components/card/Card.js
+++ components/card/Card.js
@@ -4,15 +4,22 @@ import { motion } from 'framer-motion'
 import { fadeInUp, stagger } from '../../utils/animations'
 
 export default function Card(props) {
+  console.log(props);
+  console.log(styles);
   return (
     <motion.div
       variants={stagger}
-      className={styles.container}>
+      className={`${styles.container} ${styles[props.variant]}`}
+    >
       <Link href={'hotspot/' + props.data.address}>
         <a>
+          <motion.p
+            variants={fadeInUp}
+            className={`${styles.status} ${props.data.status.online == "online" ? styles.online : styles.offline}`}>
+            {props.data.status.online ? "Online" : "Offline"}
+          </motion.p>
           <motion.h2 variants={fadeInUp}>{props.data.name}</motion.h2>
           <motion.p variants={fadeInUp}>{props.data.geocode.long_street + ", " + props.data.geocode.long_city}</motion.p>
-          <motion.p variants={fadeInUp}>Status: {props.data.status.online}</motion.p>
           <motion.p variants={fadeInUp}>Scale {props.data.reward_scale}</motion.p>
         </a>
       </Link>
