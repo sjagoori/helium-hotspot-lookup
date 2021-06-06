75d0b761697308b769302e3a7222e0200c7d5e52
sjagoori <sjagoori@users.noreply.github.com>
2021-06-06 16:01:33 +0200

    + animations

--- components/loadingScreen/loadingScreen.js
+++ components/loadingScreen/loadingScreen.js
@@ -1,14 +1,20 @@
 import styles from './LoadingScreen.module.css'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import { motion } from 'framer-motion'
+import { fadeInUp } from 'utils/animations'
 
 export default function LoadingScreen() {
   return (
     <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       className={styles.container}
+      transition={{ delay: 0.2 }}
+
     >
-      <CircularProgress />
+      <CircularProgress className={styles.loader} />
+      <motion.h1 variants={fadeInUp}>Loading</motion.h1>
     </motion.div>
   )
 }
