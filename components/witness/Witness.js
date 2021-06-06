359f6382f753c8fb378560b441024a49297f1a8d
sjagoori <sjagoori@users.noreply.github.com>
2021-06-06 20:34:19 +0200

    ~ use card component

--- components/witness/Witness.js
+++ components/witness/Witness.js
@@ -1,23 +1,13 @@
 import Header from "../header/Header";
 import Section from '../section/Section'
 import styles from './Witness.module.css'
+import Card from '../card/Card'
 
 export default function Witness(props) {
   return (
     <Section>
       <Header label="Witness" />
-      {Object.values(props.data).map((key, index) => {
-        return (
-          <a key={index} href={key.address} className={styles.card}>
-            <div>
-              <h2>{key.name}</h2>
-              <p>{key.geocode.long_street + ", " + key.geocode.long_city}</p>
-              <p>Status: {key.status.online}</p>
-              <p>Scale {key.reward_scale}</p>
-            </div>
-          </a>
-        );
-      })}
+      {Object.values(props.data).map((key, index) => <Card data={key} key={index} variant="secondary" />)}
     </Section>
   );
 }
