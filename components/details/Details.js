7ee9f1d6e1c88e5708d19597e9b63bde64914865
sjagoori <sjagoori@users.noreply.github.com>
2021-06-07 20:44:08 +0200

    + Relay report

--- components/details/Details.js
+++ components/details/Details.js
@@ -2,6 +2,9 @@ import Head from 'next/head'
 import Header from '../header/Header'
 import Section from '../section/Section'
 import BookmarkButton from 'components/bookmarkButton/BookmarkButton';
+import { motion } from 'framer-motion'
+import { fadeInUp, stagger } from '../../utils/animations'
+import styles from '../card/Card.module.css'
 
 export default function Details(props) {
   return (
@@ -10,7 +13,12 @@ export default function Details(props) {
         <title>{props.data.name}</title>
       </Head>
       <Section>
-        <Header label="Details" />
+        <div className={styles.flexbox}>
+          <Header label="Details" />
+          {props.data.status.listen_addrs != null && props.data.status.listen_addrs[0].startsWith('/p2p/') ? <motion.span
+            variants={fadeInUp}
+            className={`${styles.status} ${styles.offline}`}>Relayed</motion.span> : null}
+        </div>
         <h2>{props.data.name}</h2>
         <p>
           {props.data.geocode.long_street}, {props.data.geocode.long_state}
@@ -18,6 +26,7 @@ export default function Details(props) {
         <p>
           {props.data.geocode.long_city}, {props.data.geocode.long_country}
         </p>
+
         <BookmarkButton data={props.data} />
       </Section>
     </>
